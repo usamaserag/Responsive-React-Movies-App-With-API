ed7619ef4d97cd967cd3a44ecf6b215a5c82ae51
usamaserag <usamaserageldeen@gmail.com>
2022-01-12 23:15:43 +0200

    Styled footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './styles.scss'
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <div className="footer">
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <Link to="/">Movies</Link>
         </div>
-    )
-}
+        <div className="footer__content__menus">
+            <div className="footer__content__menu">
+                <Link to='/'>Home</Link>
+                <Link to='/'>Contact us</Link>
+                <Link to='/'>Term of services</Link>
+                <Link to='/'>About us</Link>
+            </div>
+            <div className="footer__content__menu">
+                <Link to='/'>Live</Link>
+                <Link to='/'>FAQ</Link>
+                <Link to='/'>Premium</Link>
+                <Link to='/'>Privacy policy</Link>
+            </div>
+            <div className="footer__content__menu">
+                <Link to='/'>You must watch</Link>
+                <Link to='/'>Recent release</Link>
+                <Link to='/'>Top IMDB</Link>
+            </div>
 
-export default Footer
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
